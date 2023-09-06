06f0bd1fd4a1da241fd5bc023c6e0c93eb59d4ac
Neelakandan C <neelakandan9883@outlook.com>
2023-09-06 23:29:49 +0530

    Some Changes Need to be commited

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,93 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public OrderVM OrderVM { get; set; }
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int orderId)
+        {
+            OrderVM = new()
+            {
+                OrderHead = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeproperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u=>u.OrderHeaderId == orderId, includeproperties:"Product")
+            };
+            return View(OrderVM);
+        }
+        [HttpPost]
+        [Authorize (Roles = SD.Role_User_Admin+","+ SD.Role_User_Emp) ]
+        public IActionResult UpdateOrderDatail()
+        {
+            var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHead.Id);
+
+            orderHeaderFromDb.Name = OrderVM.OrderHead.Name;
+            orderHeaderFromDb.PhoneNumber = OrderVM.OrderHead.PhoneNumber;
+            orderHeaderFromDb.StreetAdress = OrderVM.OrderHead.StreetAdress;
+            orderHeaderFromDb.City = OrderVM.OrderHead.City;
+            orderHeaderFromDb.State = OrderVM.OrderHead.State;
+            orderHeaderFromDb.Postalcode = OrderVM.OrderHead.Postalcode;
+            if (!string.IsNullOrEmpty(OrderVM.OrderHead.Carrier))
+            {
+                orderHeaderFromDb.Carrier = OrderVM.OrderHead.Carrier;
+            }
+            if (!string.IsNullOrEmpty(OrderVM.OrderHead.TrackingNumber))
+            {
+                orderHeaderFromDb.Carrier = OrderVM.OrderHead.TrackingNumber;
+            }
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+            _unitOfWork.Save();
+            TempData["success"] = "Order details Updated Successfully";
+            return RedirectToAction(nameof(Details), new {orderId=orderHeaderFromDb.Id});
+        }
+        #region APICALL
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHead> Objorderheader = _unitOfWork.OrderHeader.GetAll(includeproperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    Objorderheader = Objorderheader.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+
+                case "approved":
+                    Objorderheader = Objorderheader.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                case "completed":
+                    Objorderheader = Objorderheader.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "inprocess":
+                    Objorderheader = Objorderheader.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                default:
+                   
+                    break;
+            }
+
+            return Json(new { data = Objorderheader });
+        }
+       
+        #endregion
+    }
+}
---
+++ BulkyWeb/wwwroot/js/order.js
@@ -0,0 +1,53 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDatatable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDatatable("completed");
+        }
+        else {
+            if (url.includes("approved")) {
+                loadDatatable("approved");
+            }
+            else {
+                if (url.includes("pending")) {
+                    loadDatatable("pending");
+                }
+                else {
+                    loadDatatable("all");
+                }
+
+            }
+
+        }
+    }
+});
+
+function loadDatatable(status) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall?status='+status },       
+            "columns": [
+                { data: 'id',"width":"5%" },
+                { data: 'name', "width": "15%" },
+                { data: 'phoneNumber', "width": "15%" },
+                { data: 'applicationUser.email', "width": "20%" },
+                { data: 'orderStatus', "width": "10%" },
+                { data: 'orderTotal', "width": "15%" },
+                {
+                    data: 'id',
+                    "render": function (data) {
+                        return `<div class =w-75 btn-group role="group">
+                          <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi-pencil-square"></i></a>
+                        </div>`
+                    },
+
+                    "width": "30%"
+                }
+            ]
+    });
+}
+
